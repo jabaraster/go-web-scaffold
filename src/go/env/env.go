03e8026bd80549866fdad35925cfdea0368fee27
jabaraster <jabaraster@gmail.com>
2015-07-10 00:49:03 +0900

    mainte.

--- src/go/env/env.go
+++ src/go/env/env.go
@@ -9,57 +9,21 @@ import (
 
 const (
 	KeyMode             = "APP_MODE"
-	KeyDbKind           = "APP_DB_KIND"
-	KeyPostgresHost     = "APP_POSTGRES_HOST"
-	KeyPostgresDbName   = "APP_POSTGRES_DB_NAME"
-	KeyPostgresUser     = "APP_POSTGRES_USER"
-	KeyPostgresPassword = "APP_POSTGRES_PASSWORD"
-	KeyPostgresSslMode  = "APP_POSTGRES_SSL_MODE"
 
 	ModeProduction = "production"
 	ModeDebug      = "debug"
-
-	DbKindSQLite3    = "SQLite3"
-	DbKindPostgreSQL = "PostgreSQL"
 )
 
 var (
 	mode             string
-	dbKind           string
-	postgresHost     string
-	postgresDbName   string
-	postgresUser     string
-	postgresPassword string
-	postgresSslMode  string
 )
 
 func init() {
 	mode = getEnv(KeyMode, ModeDebug)
-
-	dbKind = getEnv(KeyDbKind, DbKindSQLite3)
-
-	if strings.EqualFold(dbKind, DbKindSQLite3) {
-		dbKind = DbKindSQLite3
-	} else if strings.EqualFold(dbKind, DbKindPostgreSQL) {
-		dbKind = DbKindPostgreSQL
-	}
-
-	postgresHost = getEnv(KeyPostgresHost, "")
-	postgresDbName = getEnv(KeyPostgresDbName, "")
-	postgresUser = getEnv(KeyPostgresUser, "")
-	postgresPassword = getEnv(KeyPostgresPassword, "")
-	postgresSslMode = getEnv(KeyPostgresSslMode, "disable")
 }
 
 func Dump() {
 	dump("mode", mode)
-	dump("dbKind", dbKind)
-
-	dump("postgresHost", postgresHost)
-	dump("postgresDbName", postgresDbName)
-	dump("postgresUser", postgresUser)
-	dump("postgresPassword", postgresPassword)
-	dump("postgresSslMode", postgresSslMode)
 }
 
 func Mode() string {
